febcf31547f549b79324edaad470bdd792e2b596
Jorge <jorge_delete@hotmail.com>
2019-04-08 15:19:11 +0100

    Final files

--- aula08/aula08.c
+++ aula08/aula08.c
@@ -1,38 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int Count = 0;
+/* alusão das funções que implementam os algoritmos pretendidos */
+/* allusion to the functions that implement the required algorithms */
+int PerrinRec (int);
+int PerrinDin (int);
 
-int PerrinRec(int n);
-int PerrinDin(int n);
+/* variável global para contar as invocações recursivas executadas pelo algoritmo */
+/* global variable for counting the recursive calls executed by the algorithm */
+int Sum;
 
-int main(void)
-{
-    // Recursivo
-    printf("================= ALGORITMO Recursivo\n");
-    for (int n = 0; n < 16; n ++ ){
-    Count = 0;
-    int r = PerrinRec(n);
-    printf("N:%d P:%d Add:%d\n", n, r, Count);
-    }
-     // Recursivo
-    printf("================= ALGORITMO Dinâmico\n");
-    for (int n = 0; n < 16; n ++ ){
-    Count = 0;
-    int r = PerrinDin(n);
-    printf("N:%d P:%d Add:%d\n", n, r, Count);
-    }
+int main (void){
+	int Result, Line, N, Test;
+
+	/* leitura do valor limite da tabela */
+	/* reading the limit value for the table */
+	do{
+		printf ("N? "); Test = scanf ("%d", &N);
+		scanf ("%*[^\n]"); scanf ("%*c");
+	} while (Test == 0);
+
+    /* impressão da tabela de execução do algoritmo */
+    /* impression of the table values for the algorithm's execution */
+	for (Line = 0; Line <= N; Line++){
+        /* inicialização da variável global contadora das adições */
+		Sum = 0;
+		
+		/* invocação do algoritmo pretendido */
+		Result = PerrinRec (Line);
+
+		/* apresentação do resultado e do número de adições executadas pelo algoritmo */
+		fprintf (stdout, "T(%2d) = %4d e fez %2d somas\n", Line, Result, Sum);
+
+	}
+	for (Line = 0; Line <= N; Line++){
+        /* inicialização da variável global contadora das adições */
+		Sum = 0;
+		
+		/* invocação do algoritmo pretendido */
+		Result = PerrinDin (Line);
+
+		/* apresentação do resultado e do número de adições executadas pelo algoritmo */
+		fprintf (stdout, "T(%2d) = %4d e fez %2d somas\n", Line, Result, Sum);
+
+	}
+	scanf ("%*c");
+	exit (EXIT_SUCCESS);
 }
+
+/* implementação dos algoritmos pretendidos */
+/* acrescentar a contagem das invocações recursivas executadas pelos algoritmos usando a variável global */
+
+/* implementation of the pretended algorithms */
+/* do not forget to count the recursive calls using the global variable */
+
 
 int PerrinRec(int n){
 	if(n < 0) return -1; // Erro
-    Count++;
     if(n == 0) return 3;
     if(n == 1) return 0;
     if(n == 2) return 2;
+    Sum++;
     return (PerrinRec(n-2) + PerrinRec(n-3));
 }
 
 int PerrinDin(int n){
-    Count++;
-    return 1;
+	if(n < 0) return -1; // Erro
+	int Array[n];
+	if(n >= 0) Array[0] = 3;
+    if(n >= 1) Array[1] = 0;
+    if(n >= 2) Array[2] = 2;
+	for(int i = 3; i <= n; i++){
+		Sum++;
+		Array[i] = Array[i-2] + Array[i-3];
+	}
+    return Array[n];
 }
